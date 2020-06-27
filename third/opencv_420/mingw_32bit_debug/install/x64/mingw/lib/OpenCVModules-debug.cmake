e1ac2c14e99b7118eb9aca044851c94216daa93d
handou1987 <3517702966@qq.com>
2020-06-27 16:09:08 +0800

    third lib: faac faad ffmepg opencv_420

---
+++ third/opencv_420/mingw_32bit_debug/install/x64/mingw/lib/OpenCVModules-debug.cmake
@@ -0,0 +1,159 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "opencv_core" for configuration "Debug"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_core420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_core420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_core )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_core "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_core420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_core420d.dll" )
+
+# Import target "opencv_flann" for configuration "Debug"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_flann420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_flann420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_flann )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_flann "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_flann420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_flann420d.dll" )
+
+# Import target "opencv_imgproc" for configuration "Debug"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_imgproc420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_imgproc420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_imgproc )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgproc "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_imgproc420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_imgproc420d.dll" )
+
+# Import target "opencv_ml" for configuration "Debug"
+set_property(TARGET opencv_ml APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_ml PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_ml420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_ml420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_ml )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_ml "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_ml420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_ml420d.dll" )
+
+# Import target "opencv_photo" for configuration "Debug"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_photo420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_photo420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_photo )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_photo "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_photo420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_photo420d.dll" )
+
+# Import target "opencv_dnn" for configuration "Debug"
+set_property(TARGET opencv_dnn APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_dnn PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_dnn420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_dnn420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_dnn )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_dnn "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_dnn420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_dnn420d.dll" )
+
+# Import target "opencv_features2d" for configuration "Debug"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_features2d420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_features2d420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_features2d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_features2d "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_features2d420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_features2d420d.dll" )
+
+# Import target "opencv_gapi" for configuration "Debug"
+set_property(TARGET opencv_gapi APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_gapi PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_gapi420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_gapi420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_gapi )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_gapi "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_gapi420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_gapi420d.dll" )
+
+# Import target "opencv_imgcodecs" for configuration "Debug"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_imgcodecs420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_imgcodecs420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_imgcodecs )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgcodecs "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_imgcodecs420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_imgcodecs420d.dll" )
+
+# Import target "opencv_videoio" for configuration "Debug"
+set_property(TARGET opencv_videoio APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_videoio PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_videoio420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_videoio420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_videoio )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_videoio "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_videoio420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_videoio420d.dll" )
+
+# Import target "opencv_calib3d" for configuration "Debug"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_calib3d420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_calib3d420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_calib3d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_calib3d "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_calib3d420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_calib3d420d.dll" )
+
+# Import target "opencv_highgui" for configuration "Debug"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_highgui420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_highgui420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_highgui )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_highgui "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_highgui420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_highgui420d.dll" )
+
+# Import target "opencv_objdetect" for configuration "Debug"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_objdetect420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_objdetect420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_objdetect )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_objdetect "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_objdetect420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_objdetect420d.dll" )
+
+# Import target "opencv_stitching" for configuration "Debug"
+set_property(TARGET opencv_stitching APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_stitching PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_stitching420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_stitching420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_stitching )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_stitching "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_stitching420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_stitching420d.dll" )
+
+# Import target "opencv_video" for configuration "Debug"
+set_property(TARGET opencv_video APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_video PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_video420d.dll.a"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_video420d.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_video )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_video "${_IMPORT_PREFIX}/x64/mingw/lib/libopencv_video420d.dll.a" "${_IMPORT_PREFIX}/x64/mingw/bin/libopencv_video420d.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
